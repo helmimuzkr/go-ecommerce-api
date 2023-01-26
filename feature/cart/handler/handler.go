05d217c578cf1966beb5ac29b5e5817fff4ee2ef
audizzy <zulfeekei@gmail.com>
2023-01-26 22:51:33 +0700

    added service, handler layer for Update, GetAll cart function

--- feature/cart/handler/handler.go
+++ feature/cart/handler/handler.go
@@ -39,15 +39,53 @@ func (ch *cartHandler) Add() echo.HandlerFunc {
 
 // Delete implements cart.CartHandler
 func (ch *cartHandler) Delete() echo.HandlerFunc {
-	panic("unimplemented")
+	return func(c echo.Context) error {
+		userID := c.Get("userID").(uint)
+		productID := c.Param("productID")
+		id, err := strconv.Atoi(productID)
+		if err != nil {
+			return c.JSON(helper.ErrorResponse("Kesalahan pada input user"))
+		}
+
+		err = ch.srv.Delete(userID, uint(id))
+		if err != nil {
+			return c.JSON(helper.ErrorResponse(err.Error()))
+		}
+		return c.JSON(helper.SuccessResponse(http.StatusOK, "berhasil delete cart"))
+	}
 }
 
 // GetAll implements cart.CartHandler
 func (ch *cartHandler) GetAll() echo.HandlerFunc {
-	panic("unimplemented")
+	return func(c echo.Context) error {
+		userID := c.Get("userID").(uint)
+
+		carts, err := ch.srv.GetAll(userID)
+		if err != nil {
+			return c.JSON(helper.ErrorResponse(err.Error()))
+		}
+		return c.JSON(helper.SuccessResponse(http.StatusOK, "berhasil menunjukkan cart", carts))
+	}
 }
 
 // Update implements cart.CartHandler
 func (ch *cartHandler) Update() echo.HandlerFunc {
-	panic("unimplemented")
+	return func(c echo.Context) error {
+		userID := c.Get("userID").(uint)
+		productID := c.Param("productID")
+		quantity, err := strconv.Atoi(c.FormValue("quantity"))
+		if err != nil {
+			return c.JSON(helper.ErrorResponse("Kesalahan pada input user"))
+		}
+		id, err := strconv.Atoi(productID)
+		if err != nil {
+			return c.JSON(helper.ErrorResponse("Kesalahan pada input user"))
+		}
+
+		err = ch.srv.Update(userID, uint(id), quantity)
+		if err != nil {
+			return c.JSON(helper.ErrorResponse(err.Error()))
+		}
+		return c.JSON(helper.SuccessResponse(http.StatusOK, "berhasil update cart"))
+	}
 }
