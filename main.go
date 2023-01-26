50b9c00a20f658564ce1b15e559589c4661a9e93
helmimuzkr <mhelmimuzakir@gmail.com>
2023-01-26 13:49:59 +0700

    feat: setup create order feautre

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"e-commerce-api/config"
+	_orderData "e-commerce-api/feature/order/data"
+	_orderHandler "e-commerce-api/feature/order/handler"
+	_orderService "e-commerce-api/feature/order/service"
 	_productData "e-commerce-api/feature/product/data"
 	_productHandler "e-commerce-api/feature/product/handler"
 	_productService "e-commerce-api/feature/product/service"
@@ -25,6 +28,7 @@ func main() {
 
 	v := validator.New()
 	cld := config.NewCloudinary(*cfg)
+	s := config.NewSnapMidtrans(*cfg)
 
 	productData := _productData.New(db)
 	productService := _productService.New(productData, v, cld)
@@ -33,6 +37,10 @@ func main() {
 	userData := data.New(db)
 	userSrv := services.New(userData)
 	userHdl := handler.New(userSrv)
+
+	orderData := _orderData.New(db)
+	orderService := _orderService.New(orderData, s)
+	orderHandler := _orderHandler.New(orderService)
 
 	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(middleware.CORS())
@@ -52,7 +60,8 @@ func main() {
 	e.PUT("/products/:product_id", productHandler.Update(), middleware.JWT([]byte(config.JWT_KEY)))
 	e.DELETE("/products/:product_id", productHandler.Delete(), middleware.JWT([]byte(config.JWT_KEY)))
 
-	e.POST("/products", productHandler.Add(), middleware.JWT([]byte(config.JWT_KEY)))
+	e.POST("/orders", orderHandler.Create(), middleware.JWT([]byte(config.JWT_KEY)))
+
 	if err := e.Start(":8000"); err != nil {
 		log.Println(err.Error())
 	}
